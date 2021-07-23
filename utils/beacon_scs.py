d91868744a14f16c90e6c0cf738f0482dc0de4c8
ryanpdx <rmedick@pdx.edu>
2021-07-23 16:18:10 -0700

    added a thread for telecomands for beacon scs script

--- utils/beacon_scs.py
+++ utils/beacon_scs.py
@@ -3,10 +3,13 @@
 
 import sys
 import socket
+from threading import Thread
 from serial import Serial, SerialException
 
+TTY = '/dev/serial/by-id/usb-SCS_SCS_Tracker___DSP_TNC_PT2HJ743-if00-port0'
 
-def _readline(ser):
+
+def _readline(ser: Serial):
     eol = b'\r\n'
     leneol = len(eol)
     line = bytearray()
@@ -21,10 +24,7 @@ def _readline(ser):
     return bytes(line)
 
 
-def main():
-    # open serial
-    tty = '/dev/serial/by-id/usb-SCS_SCS_Tracker___DSP_TNC_PT2HJ743-if00-port0'
-    ser = Serial(tty, 38400, timeout=15.0)
+def send_tm(ser: Serial):
     tm_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     # CSC extracts the header and send it as a seperate message decode
@@ -52,13 +52,26 @@ def main():
         # merge header and payload together
         # strip the carriage return and newline.
         packet = packet_header + line[:len(line)-2]
-        print(packet)
-        print("")
 
         tm_socket.sendto(packet, ('127.0.0.1', 10015))
 
 
-try:
-    main()
-except KeyboardInterrupt:
-    sys.exit()
+def receive_tc(ser: Serial):
+    tc_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    tc_socket.bind(('127.0.0.1', 10025))
+    while True:
+        data, _ = tc_socket.recvfrom(4096)
+        ser.write(data)
+
+
+if __name__ == '__main__':
+    ser = Serial(TTY, 38400, timeout=15.0)
+
+    tm_thread = Thread(target=send_tm, args=(ser,))
+    tm_thread.daemon = True
+
+    try:
+        tm_thread.start()
+        receive_tc(ser)
+    except KeyboardInterrupt:
+        sys.exit(0)
