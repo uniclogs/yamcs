6cd28274c623e677ea349130e1234cfe488554c1
Dmitri McGuckin <dmitri@duck.com>
2025-02-02 18:08:25 -0700

    Culled a buncha files

--- src/main/java/org/oresat/uniclogs/tctm/EdlCommandPostprocessor.java
+++ src/main/java/org/oresat/uniclogs/tctm/EdlCommandPostprocessor.java
@@ -1,25 +1,21 @@
 package org.oresat.uniclogs.tctm;
-import org.oresat.uniclogs.services.UniclogsEnvironment;
-import org.yamcs.ConfigurationException;
+
 import org.yamcs.YConfiguration;
-import org.yamcs.YamcsServer;
 import org.yamcs.cmdhistory.CommandHistoryPublisher;
 import org.yamcs.commanding.PreparedCommand;
 import org.yamcs.tctm.CommandPostprocessor;
 
-
 public class EdlCommandPostprocessor implements CommandPostprocessor {
     protected CommandHistoryPublisher cmdHistory;
-    UniclogsEnvironment env;
 
     @Override
     public byte[] process(PreparedCommand pc) {
-        Integer seqNum = this.env.getSeqNum();
-        byte[] hmacKey = this.env.getHmacKey();
+        Integer seqNum = null;
+        byte[] hmacKey = null;
 
         EDLPacket cmd = new EDLPacket(pc.getBinary(), seqNum, hmacKey);
         byte[] cmdBin = cmd.getBinary();
-        
+
         pc.setBinary(cmdBin);
         this.cmdHistory.publish(pc.getCommandId(), "edl-seqnum", seqNum);
         this.cmdHistory.publish(pc.getCommandId(), PreparedCommand.CNAME_BINARY, cmdBin);
@@ -32,16 +28,11 @@ public class EdlCommandPostprocessor implements CommandPostprocessor {
     }
 
     public EdlCommandPostprocessor(String instanceName, YConfiguration config) {
-        String envName = config.getString("envService");
-        this.env = YamcsServer.getServer().getInstance(instanceName).getService(UniclogsEnvironment.class, envName);
-        
-        if (this.env == null) {
-            throw new ConfigurationException("Service " + envName + " does not exist or is not of class UniclogsEnvironment.");
-        }
+
     }
 
     public EdlCommandPostprocessor(String instanceName) {
         this(instanceName, YConfiguration.emptyConfig());
     }
-    
+
 }
