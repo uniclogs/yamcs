f6f017219f16a0101ea06eeaa32ee5c85b822848
Dmitri McGuckin <dmitri@duck.com>
2025-02-23 19:01:31 -0700

    Cleanup imports

--- src/main/java/org/uniclogs/yamcs/tctm/BeaconPacket.java
+++ src/main/java/org/uniclogs/yamcs/tctm/BeaconPacket.java
@@ -1,9 +1,6 @@
 package org.uniclogs.yamcs.tctm;
 
-import java.util.Arrays;
-
 import org.yamcs.TmPacket;
-import org.yamcs.xtce.util.HexUtils;
 
 public class BeaconPacket extends Packet {
     private static final Integer SEQ_NUM_OFFSET = 26;
@@ -12,7 +9,7 @@ public class BeaconPacket extends Packet {
         super(tmPacket.getPacket(), tmPacket.getPacket().length,
                 getSequenceNumber(tmPacket.getPacket(), SEQ_NUM_OFFSET), SEQ_NUM_OFFSET);
 
-        byte[] pkt = tmPacket.getPacket();
+        // byte[] pkt = tmPacket.getPacket();
         // log.info(HexUtils.hex(Arrays.copyOfRange(pkt, 0, pkt.length - 4)));
         // log.info(HexUtils.hex(Arrays.copyOfRange(pkt, pkt.length - 4, pkt.length)));
         // log.info(HexUtils.hex(Arrays.copyOfRange(pkt, 0, pkt.length)));
